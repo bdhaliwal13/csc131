6fe61129bbb8bb80a12479faea5015d35076e3ce
charvue <49206683+charvue@users.noreply.github.com>
2019-04-04 22:37:53 -0700

    added line

--- Lab2/src/Hello.java
+++ Lab2/src/Hello.java
@@ -1,4 +1,20 @@
-
 public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Git!");
+		System.out.println("yang - This line was added by Dr. Yang.");
+		// Add a new line with your information
+		System.out.println("218897095 - This line was added by Charlie Vue.");
+		System.out.println("213584255 - This line was added by Brinder Dhaliwal.");
+		System.out.println("219988042 - This line was added by Casey Whitsell.");
+		System.out.println("219495342 - This line was added by Tanner Fleming.");
+		System.out.println("218902477 - This line was added by Justin Roldan.");
+		System.out.println("219437869 - This line was added by Paul Davydov.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+
+}
 
 }
