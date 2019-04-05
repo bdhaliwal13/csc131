33804de9a104b56cad412959f536d25adba8eeb2
roldanj <roldanj@WALL.ecs.csus.edu>
2019-04-04 18:52:38 -0700

    Added my name and ID

--- Lab02/src/Hello.java
+++ Lab02/src/Hello.java
@@ -8,7 +8,7 @@ public class Hello {
 		System.out.println("213584255 - This line was added by Brinder Dhaliwal.");
 		System.out.println("219988042 - This line was added by Casey Whitsell.");
 		System.out.println("219495342 - This line was added by Tanner Fleming.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("218902477 - This line was added by Justin Roldan.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
